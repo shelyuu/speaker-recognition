d858be0fb12f4b846f98fe80bedd95f811b67299
shelyuu <shelyuu@gmail.com>
2024-06-30 23:55:37 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+
+### References
+- [Sound Classification by Juan Pablo Bello](https://s18798.pcdn.co/jpbello/wp-content/uploads/sites/1691/2018/01/8-classification.pdf)
+- 
---
+++ speech_features_extraction.py
@@ -0,0 +1,277 @@
+import os
+import random as rand
+os.environ['TF_ENABLE_ONEDNN_OPTS'] = '0'
+import keras
+import tensorflow as tf
+import numpy as np
+import pandas as pd
+import librosa
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn.metrics import ConfusionMatrixDisplay, confusion_matrix
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Dropout
+from tensorflow.keras.callbacks import History
+
+def extract_features(file_path):
+	# Load audio file, refer to https://librosa.org/doc/main/generated/librosa.resample.html for res_type
+	audio, sample_rate = librosa.load(file_path, res_type='kaiser_best')
+ 
+ 	# Chromas
+	stft = np.abs(librosa.stft(audio))
+	chroma_stft = np.mean(librosa.feature.chroma_stft(S=stft, sr=sample_rate).T, axis=0)
+
+	cqt = np.abs(librosa.cqt(y=audio, sr=sample_rate))
+	# chroma_cqt = np.mean(librosa.feature.chroma_cqt(C=cqt, sr=sample_rate).T, axis=0)
+
+	# vqt = np.abs(librosa.vqt(audio, sr=sample_rate))
+	# chroma_vqt = np.mean(librosa.feature.chroma_vqt(V=vqt, sr=sample_rate).T, axis=0)
+
+	chroma_cens = np.mean(librosa.feature.chroma_cens(C=cqt, sr=sample_rate).T, axis=0)
+
+    # Extract Mel Spectrogram
+	mel_spectrogram = np.mean(librosa.feature.melspectrogram(y=audio, sr=sample_rate).T, axis=0)
+    
+	# Mel-Frequency Cepstral Coefficients
+	mfccs = librosa.feature.mfcc(y=audio, sr=sample_rate, n_mfcc=40)
+	# Standardization: The first approach ((mfccs - np.mean(mfccs, axis=1, keepdims=True)) / (np.std(mfccs, axis=1, keepdims=True) + 1e-6)) is used to standardize the MFCC features, maintaining their temporal structure while normalizing their scales.
+	# Averaging: The second approach (np.mean(mfccs.T, axis=0)) simply averages the MFCC features over time, losing the temporal information but providing a single summary vector.
+	mfccs_scaled = (mfccs - np.mean(mfccs, axis=1, keepdims=True)) / (np.std(mfccs, axis=1, keepdims=True) + 1e-6)
+	# mfccs_scaled = np.mean(mfccs.T, axis=0)
+	mfccs_mean = np.mean(mfccs_scaled.T, axis=0)
+
+	# root-mean-square
+	rms = np.mean(librosa.feature.rms(y=audio).T, axis=0)
+ 
+	# spectral centroid
+	spectral_centroid = np.mean(librosa.feature.spectral_centroid(y=audio, sr=sample_rate).T, axis=0)
+ 
+	# polynomial features
+	poly_features = np.mean(librosa.feature.poly_features(y=audio, sr=sample_rate).T, axis=0)
+ 
+ 	#	tonal centroid features 
+	tonnetz = np.mean(librosa.feature.tonnetz(y=audio, sr=sample_rate).T, axis=0)
+ 
+  	# Zero-Crossing Rate
+	zcr = np.mean(librosa.feature.zero_crossing_rate(y=audio).T, axis=0)
+
+	# Stack features
+	features = np.hstack([
+		# chroma_stft,
+		chroma_cens,
+		mel_spectrogram,
+		mfccs_mean,
+		rms,
+		# spectral_centroid,
+		# poly_features,
+		# tonnetz,
+		zcr
+	])
+
+	# features = np.vstack([
+	# 	zcr,
+	# 	mfcc,
+	# 	mel_spectrogram,
+	# 	chroma_stft
+	# ])
+ 
+	# stacked_features = librosa.feature.stack_memory(features.T, n_steps=5)
+    
+	return features
+
+def load_data(dataset_path):
+    features = []
+    labels = []
+    for speaker in os.listdir(dataset_path):
+        speaker_path = os.path.join(dataset_path, speaker)
+        if os.path.isdir(speaker_path):
+            for file_name in os.listdir(speaker_path):
+                file_path = os.path.join(speaker_path, file_name)
+                if file_path.endswith('.flac'):
+                    feature = extract_features(file_path)
+                    features.append(feature)
+                    labels.append(speaker)
+    return np.array(features), np.array(labels)
+
+def predict_speaker(file_path, model, le):
+    feature = extract_features(file_path).reshape(1, -1)
+    prediction = model.predict(feature)
+    predicted_label = le.inverse_transform([np.argmax(prediction)])
+    return predicted_label[0]
+
+def test_model(test_model_path):
+	model = tf.keras.models.load_model(test_model_path)
+	predicted_speaker = predict_speaker(audio_file_path, model, le)
+	print(f"Predicted Speaker ({audio_filename}): {predicted_speaker}")
+
+# ===========================================
+#
+# settings
+#
+# ===========================================
+
+mode = "train" # "train" or "analyse" or "test"
+speaker = 1 # speaker identity for testing
+max_iter = 1 # iteration (usually for training model with random seed)
+show_graphs = True
+# end settings	=====================
+
+
+figsz_config = (8, 3)
+audio_filename = f"{speaker}-{rand.randint(1, 10)}.flac"
+audio_file_path = f"audio\speakers\Mendeley Data\samePhrase\\{speaker}\\{audio_filename}"
+model_path = "model\\"
+test_model_path = "model\speaker_recognition_model.keras"
+dataset_path = "audio\speakers\Mendeley Data\differentPhrase"
+
+accuracy = 1.0
+if mode == "train":
+	accuracy = 0.0
+iter = 0
+
+X, y = load_data(dataset_path)
+le = LabelEncoder()
+y_encoded = le.fit_transform(y)
+
+if mode == "analyse":
+	audio_path = audio_file_path
+	audio, sample_rate = librosa.load(audio_path)
+	
+	audio_name = os.path.basename(audio_path)
+	print(f"{audio_name} is chosen...")
+ 
+	#Waveform Plot
+	plt.figure(figsize=figsz_config)
+	plt.plot(audio)
+	plt.title('Waveform')
+	plt.xlabel('Time')
+	plt.ylabel('Amplitude')
+	
+	#Spectrogram
+	plt.figure(figsize=figsz_config)
+	D = librosa.amplitude_to_db(np.abs(librosa.stft(audio)), ref=np.max)
+	librosa.display.specshow(D, sr=sample_rate, x_axis='time', y_axis='log')
+	plt.colorbar(format='%+2.0f dB')
+	plt.title('Spectrogram')
+	
+	#MFCCs
+	mfccs = librosa.feature.mfcc(y=audio, sr=sample_rate, n_mfcc=13)
+
+	plt.figure(figsize=figsz_config)
+	librosa.display.specshow(mfccs, sr=sample_rate, x_axis='time')
+	plt.colorbar()
+	plt.title('MFCC')
+	plt.tight_layout()
+	
+	#Chroma Features
+	chroma = librosa.feature.chroma_stft(y=audio, sr=sample_rate)
+
+	plt.figure(figsize=figsz_config)
+	librosa.display.specshow(chroma, sr=sample_rate, x_axis='time', y_axis='chroma')
+	plt.colorbar()
+	plt.title('Chroma Features')
+	plt.tight_layout()
+ 
+	#Zero-Crossing Rate
+	zcr = librosa.feature.zero_crossing_rate(y=audio)
+	plt.figure(figsize=figsz_config)
+	plt.plot(zcr[0])
+	plt.title('Zero-Crossing Rate')
+	plt.xlabel('Time')
+	plt.ylabel('Rate')
+ 
+	#Class Distribution
+	labels = y_encoded
+	label_df = pd.DataFrame(labels, columns=['class'])
+
+	plt.figure(figsize=figsz_config)
+	sns.countplot(x='class', data=label_df)
+	plt.title('Class Distribution')
+	plt.xlabel('Class')
+	plt.ylabel('Count')
+ 
+    # Frequency Analysis
+	fft = np.fft.fft(audio)
+	magnitude = np.abs(fft)
+	frequency = np.linspace(0, sample_rate, len(magnitude))
+	plt.figure(figsize=figsz_config)
+	# this math plot only the positive frequencies
+	plt.plot(frequency[:len(frequency)//2], magnitude[:len(magnitude)//2], color='blue', linestyle='-', linewidth=1)
+	plt.title('Frequency Spectrum')
+	plt.xlabel('Frequency (Hz)')
+	plt.ylabel('Magnitude')
+	plt.grid(True)
+
+	plt.show()
+ 
+else: 
+	while accuracy <= 0.85 and iter < max_iter:
+		iter+=1
+		random_seed = rand.randint(0, 9999)
+		if mode == "train":
+			X_train, X_test, y_train, y_test = train_test_split(X, y_encoded, test_size=0.6, random_state=random_seed)
+
+			df = pd.DataFrame(X)
+			df['label'] = y_encoded
+			df.to_csv('features_labels.csv', index=False)
+
+			# Dense layer: a fully connected layer with an input shape of X_train.shape[1] and 256 neurons
+			# activation function: relu (Rectified Linear Unit)
+			# Input shape: The input shape of a Dense layer represents the shape of the input that the layer expects to receive.
+			#               In this case, the input shape is X_train.shape[1], which is the number of features in each sample.
+			# Activation function: The activation function is used to introduce non-linearity into the model.
+			#                      In this case, we use the ReLU activation function, which is computationally efficient and widely used.
+	
+			# Dropout layer: This layer randomly sets a fraction of input units to 0 at each update during training time, which helps prevent overfitting.
+			#                Here, we set the dropout rate to 0.3, meaning 30% of the input units will be randomly set to 0 at each update.
+			model = Sequential()
+			model.add(Dense(256, input_shape=(X_train.shape[1],), activation='relu'))
+			model.add(Dropout(0.3))
+			model.add(Dense(128, activation='relu'))
+			model.add(Dropout(0.3))
+			
+			# Dense layer: This is the output layer of the model. It has the same number of neurons as the number of unique labels in y_encoded.
+			#               The activation function is softmax, which is used for multi-class classification problems.
+			#               The softmax function transforms the output of each neuron into a probability distribution over all classes.
+			model.add(Dense(len(np.unique(y_encoded)), activation='softmax'))
+	
+			model.compile(loss='sparse_categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+			history = model.fit(X_train, y_train, epochs=50, batch_size=32, validation_data=(X_test, y_test))
+   
+			loss, accuracy = model.evaluate(X_test, y_test)
+			print(f"Test Accuracy: {accuracy * 100:.2f}%")
+		
+			new_model_path = model_path + f"speaker_recognition_model_{random_seed}.keras"
+			if accuracy > 0.85:
+				keras.saving.save_model(model, new_model_path)
+				test_model(new_model_path)
+				print(f"Model saved at: {new_model_path}")
+    
+			if show_graphs:
+				# Plot training & validation loss values
+				plt.figure(figsize=figsz_config)
+				plt.plot(history.history['loss'], label='Train Loss')
+				plt.plot(history.history['val_loss'], label='Validation Loss')
+				plt.title('Model Loss')
+				plt.xlabel('Epoch')
+				plt.ylabel('Loss')
+				plt.legend(loc='upper right')
+				plt.grid(True)
+	
+				# Make predictions on the test set
+				y_pred = model.predict(X_test)
+				y_pred_classes = np.argmax(y_pred, axis=1)
+
+				# Plot the confusion matrix
+				cm = confusion_matrix(y_test, y_pred_classes)
+				disp = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=le.classes_)
+				disp.plot(cmap=plt.cm.Blues)
+				plt.title("Confusion Matrix")
+				plt.show()
+	
+		elif os.path.exists(test_model_path) and mode == "test":
+			test_model(test_model_path)
+		else:
+			print("Model not found. Please train the model first.")
